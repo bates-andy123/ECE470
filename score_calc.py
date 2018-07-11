21a32dc3d6c94ce20a4f85ac6cc199373379d6cc
Warren-K <41024707+Warren-K@users.noreply.github.com>
2018-07-11 16:36:44 -0700

    renamed better

--- score_calc.py
+++ score_calc.py
@@ -2,7 +2,7 @@ import result
 
 #find 5th array of list 'year' in RESULT
 #Placement 5 = 1st place, 4 = 2nd, etc.
-def get_winner(year,placement):
+def get_place(year,placement):
 	print result.RESULT[year][placement]
     pass
 	
