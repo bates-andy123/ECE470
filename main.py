6c4bb9f11b786123c1c992ab5d7d60ec42515a23
Andy Bates <bates.andy123@gmail.com>
2018-07-11 09:47:16 -0700

    merged in Kierans Code

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ _2010 = data.Data("./dataset/2010.csv")
 
 #_2010.print_raw_data()
 
-score_calc.get_winner(2011)
+score_calc.get_place(2011, 5)
--- score_calc.py
+++ score_calc.py
@@ -3,8 +3,4 @@ import result
 #find 5th array of list 'year' in RESULT
 #Placement 5 = 1st place, 4 = 2nd, etc.
 def get_place(year,placement):
-	print result.RESULT[year][placement]
-    pass
-	
-	
-def_
+    print result.RESULT[year][placement]
